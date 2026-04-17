3ea03fbd86c930cb16f48bc425833529134613f5
Igoriok37 <igrina73@mail.ru>
2026-04-17 23:43:53 +0300

    Delete js directory

--- js/project.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Передаем ID текущего проекта, чтобы он не попал в рекомендации
-  if (window.projectFilter) {
-      window.projectFilter.initRecommended(1); // 1 - это ID текущей виллы
-  }
-});
